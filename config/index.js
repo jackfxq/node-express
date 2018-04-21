b26db574742b1ec706ea609111dc6e5b6f4f29a2
jack <jxsfxq@126.com>
2018-04-21 21:25:46 +0800

    test

--- config/index.js
+++ config/index.js
@@ -1,5 +1,2 @@
 module.exports={
-    mongodb:{
-        connect:'mongodb://blog:123456@111.230.35.213:27017/blog'
-    }
 }
--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config=require('../config');
+const config=require('../../mongodb.js');
 const Schema = mongoose.Schema;
 console.log(config.mongodb.connect)
 
