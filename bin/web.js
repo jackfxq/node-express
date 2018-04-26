362095f1ba0153861c45fc88cb5b214d65c0c450
duoyi <jxsfxq@126.com>
2018-04-26 11:35:12 +0800

    test

--- bin/web.js
+++ bin/web.js
@@ -12,7 +12,7 @@ app.all('*', function(req, res, next) {
     
     if(config.accessControlAllowOrigin.includes(req.get('Origin'))){
         res.header("Access-Control-Allow-Origin", req.get('Origin'));
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
         res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
         res.header("X-Powered-By",' 3.2.1')
         res.header("Content-Type", "application/json;charset=utf-8");
